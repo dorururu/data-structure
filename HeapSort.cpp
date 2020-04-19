0a3bd730922eb5d136a237396fa6a27351741b09
혜원 <askyox7595@gmail.com>
2020-04-19 17:28:29 +0900

    Heap Sort (Min Heap_ascending order)

---
+++ HeapSort.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#define MAX 100
+using namespace std;
+
+
+typedef int PriorityComp (int d1, int d2);
+
+struct Heap {
+	PriorityComp *comp;
+	int numOfData;
+	int heapArr[MAX];
+};
+
+void heapInit(Heap *ph, PriorityComp pc) {
+	ph->numOfData = 0;
+	ph->comp = pc;
+}
+
+int isEmpty(Heap *ph) {
+	if(ph->numOfData == 0)
+		return true;
+	else
+		return false;
+}
+
+int getParent(int idx) {
+	return idx/2;
+}
+
+int getLeftChild(int idx) {
+	return idx*2;
+}
+
+int getRightChild(int idx) {
+	return idx*2+1;
+}
+
+int getHighPriorityChild(Heap *ph, int idx) {
+	int leftChild = getLeftChild(idx);
+	int rightChild = getRightChild(idx);
+
+	if(leftChild > ph->numOfData) {
+		return 0;
+	} else if(leftChild == ph->numOfData) {
+		return leftChild;
+	} else {
+		if(ph->comp(ph->heapArr[leftChild], ph->heapArr[rightChild]) < 0)
+			return rightChild;
+		else
+			return leftChild; 
+	}
+}
+
+void insert(Heap *ph, int data) {
+	int idx = ph->numOfData + 1;
+
+	int parentIdx;
+	while(idx != 1) {
+		parentIdx = getParent(idx);
+		if(ph->comp(data, ph->heapArr[parentIdx]) > 0) {
+			ph->heapArr[idx] = ph->heapArr[parentIdx];
+			idx = parentIdx;
+		}
+		else break;
+	}
+
+	ph->heapArr[idx] = data;
+	ph->numOfData += 1;
+}
+
+int pop(Heap *ph) {
+	int rdata = ph->heapArr[1];
+	int lastElem = ph->heapArr[ph->numOfData];
+
+	int parentIdx = 1;
+	int childIdx;
+
+	while(childIdx = getHighPriorityChild(ph, parentIdx)) {
+		if(ph->comp(lastElem, ph->heapArr[childIdx]) >= 0) break;
+
+		ph->heapArr[parentIdx] = ph->heapArr[childIdx];
+		parentIdx = childIdx;
+	}
+
+	ph->heapArr[parentIdx] = lastElem;
+	ph->numOfData -= 1;
+	return rdata;
+}
+
+int pcmp(int num1, int num2) {
+	return num2 - num1;
+}
+
+void heapSort(int arr[], int n, PriorityComp pc) {
+	Heap heap;
+	int i;
+
+	heapInit(&heap, pcmp);
+
+	for(i=0; i<n; i++) insert(&heap, arr[i]);
+
+	for(i=0; i<n; i++) arr[i] = pop(&heap);
+}
+
+
+int main() {
+
+	int n = 4;
+	int arr[] = {3, 4, 2, 1};
+
+	heapSort(arr, n, pcmp);
+
+	for(int i=0; i<n; i++) cout <<  arr[i] << ' ';
+
+	return 0;
+}
